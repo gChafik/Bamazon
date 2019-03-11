0405c3d0e19d67ed6b221ddcb2141ffb19aa7201
Chafik <gchafik@gmail.com>
2019-03-11 07:15:15 -0700

    bamazon customer

---
+++ bamazonCustomer.js
@@ -0,0 +1,73 @@
+let mysql = require ("mysql");
+let inquirer = require ("inquirer");
+
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "bamazon"
+});
+
+connection.connect(function(err){
+    if (err) throw err;
+    displayItems();
+    placeOrder();
+});
+
+function displayItems(){
+    connection.query("SELECT * FROM PRODUCTS", function(err, res){
+        if (err) throw err;
+        console.log(res);
+        //connection.end();
+    });
+}
+function placeOrder(){
+    inquirer.prompt([{
+        name: "item_id",
+        type: "input",
+        message: "Please enter the product ID you would like to purchase"
+    },
+    {
+        name: "quantity",
+        type: "input",
+        message: "How many units would you like to buy?"
+    }
+    ])
+    .then(function(answer){
+    connection.query("SELECT * FROM PRODUCTS WHERE ?", {item_id: answer.item_id},function(err, res){
+        if (err) throw err;
+        console.log("Stock Quantity : ", res[0].stock_quantity);
+        console.log("User Input: ", answer.quantity);
+        console.log("Price: ", res[0].price);
+        if(answer.quantity > res[0].stock_quantity){
+            console.log("Insufficient quantity!")
+            connection.end();
+        }
+        else{
+            // console.log("Response: ", res[0]);
+            // console.log("Item ID" , res[0].item_id);
+            // console.log("Stock Quantity" , res[0].stock_quantity);
+            // console.log("Price" , res[0].price);
+            console.log("Total Cost of Purchase: ", answer.quantity * res[0].price);
+
+            //update product table
+            let new_stock_quantity = res[0].stock_quantity - answer.quantity;
+            connection.query("UPDATE PRODUCTS SET ? WHERE ?", 
+                [{
+                    stock_quantity : new_stock_quantity
+                },
+                {
+                    item_id : res[0].item_id
+                }],
+                function(err){
+                    if (err) throw err;
+                    console.log("Order placed successfully!");
+                }
+            );
+            connection.end();
+        }
+    });
+});
+
+}
